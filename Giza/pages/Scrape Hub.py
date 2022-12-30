62b587669004aa866f2a299c8bce68ccd506399f
Brayden-L <braydenmlevy@gmail.com>
2022-12-29 23:25:54 -0800

    make scrape download button use proper df

--- Giza/pages/Scrape Hub.py
+++ Giza/pages/Scrape Hub.py
@@ -24,14 +24,15 @@ if 'list_type' not in st.session_state:
     st.session_state.list_type = "Ticks"
 
 st.warning("Scraping is time intensive (~1s/route) and can take as long as 20min for a route list of ~1000. If you're mostly interested in exploring the functionality, try a provided dataset.", icon="⚠️")
+st.header('Select Type')
+list_type_options = ["Ticks", "ToDos"]
+st.session_state.list_type = st.radio("List Type", options=list_type_options, horizontal=True, on_change=disable_buttons, index=list_type_options.index(st.session_state.list_type))
 col1, col2, col3 = st.columns([1,1,1])
 with col1:
     st.header('1. Download')
-    list_type_options = ["Ticks", "ToDos"]
-    st.session_state.list_type = st.radio("List Type", options=list_type_options, horizontal=True, on_change=disable_buttons, index=list_type_options.index(st.session_state.list_type))
     upload_link = st.text_input("Climber Profile Link", value='https://www.mountainproject.com/user/14015/nick-wilder', placeholder='https://www.mountainproject.com/user/14015/nick-wilder')
     st.session_state.upload_link_ref = upload_link.split('/')[5]
-    if st.button("Download Data"):
+    if st.button(f"Download {st.session_state.list_type} Data"):
         st.session_state['scrape_button_state'] = True
         st.session_state['dl_button_state'] = True
         with st.spinner("Downloading"):
@@ -55,7 +56,7 @@ with col2:
     numrows = len(st.session_state.df_usend_uniq.index)
     if numrows:
         st.session_state.scrape_cutoff = st.slider("""Scrape The "N" Most Recent Routes""", min_value=0, max_value=numrows, value=numrows)
-    if st.button("Scrape", disabled=st.session_state.scrape_button_state): # TODO Rerunning scrapes to fill missing values works in jupyter but not streamlit. The column is being re-cast to string for some reason upon page rerun.
+    if st.button(f"Scrape {st.session_state.list_type}", disabled=st.session_state.scrape_button_state): # TODO Rerunning scrapes to fill missing values works in jupyter but not streamlit. The column is being re-cast to string for some reason upon page rerun.
         st.session_state.dl_button_state = False
         st.session_state.df_usend_uniq.drop(st.session_state.df_usend_uniq.iloc[st.session_state.scrape_cutoff:].index, inplace=True)
         st.session_state.df_usend_uniq = routescrape_syncro(st.session_state.df_usend_uniq)
@@ -89,16 +90,20 @@ with col2:
                 st.info("To retry, redownload the data then scrape again.", icon="ℹ️")
 st.markdown('---')
 st.header('3. Export')
+if st.session_state.list_type == "Ticks":
+    dl_val = st.session_state.df_usend_uniq_ticks
+if st.session_state.list_type == "ToDos":
+    dl_val = st.session_state.df_usend_uniq_todos
 st.download_button(
-    label="Download .PKL File For Giza",
-    data=pickle.dumps(st.session_state.df_usend_uniq),
+    label=f"Download {st.session_state.list_type} .PKL File For Giza",
+    data=pickle.dumps(dl_val),
     file_name=f'scraped_climbs_{st.session_state.list_type.lower()}_{st.session_state.upload_link_ref}.pkl',
     disabled=st.session_state.dl_button_state,
     help="PKL files are used for further analysis within Giza"
 )  
 st.download_button(
-    label="Download .CSV For Personal Use",
-    data=st.session_state.df_usend_uniq.to_csv().encode('utf-8'),
+    label=f"Download {st.session_state.list_type} .CSV For Personal Use",
+    data=dl_val.to_csv().encode('utf-8'),
     file_name=f'scraped_climbs_{st.session_state.list_type.lower()}_{st.session_state.upload_link_ref}.csv',
     mime='text/csv',
     disabled=st.session_state.dl_button_state,
