20659e60120bfd0cb6b055b39105b49587eb07ff
Brayden-L <braydenmlevy@gmail.com>
2023-12-14 21:35:29 -0800

    add session state initialization to scrape page

--- Giza/pages/1_Scrape.py
+++ Giza/pages/1_Scrape.py
@@ -15,6 +15,35 @@ def disable_buttons():
     st.session_state.exp_button_state = True
     st.session_state.df_usend_uniq = pd.DataFrame()
 
+def session_state_init():
+    if "scrape_button_state" not in st.session_state:
+        st.session_state.scrape_button_state = True
+    if "exp_button_state" not in st.session_state:
+        st.session_state.exp_button_state = True
+    if "df_usend" not in st.session_state:
+        st.session_state.df_usend = pd.DataFrame()
+    if "df_usend_uniq" not in st.session_state:
+        st.session_state.df_usend_uniq = pd.DataFrame()
+    if "df_usend_uniq_ticks" not in st.session_state:
+        st.session_state.df_usend_uniq_ticks = pd.DataFrame()
+    if "df_usend_uniq_todos" not in st.session_state:
+        st.session_state.df_usend_uniq_todos = pd.DataFrame()
+    if "list_type" not in st.session_state:
+        st.session_state.list_type = "Ticks"
+    if "df_ticks_fil" not in st.session_state:
+        st.session_state.df_ticks_fil = pd.DataFrame()
+    if "tick_upload_link_ref" not in st.session_state:
+        st.session_state.tick_upload_link_ref = ""
+    if "todo_upload_link_ref" not in st.session_state:
+        st.session_state.todo_upload_link_ref = ""
+    if "upload_link_ref_str" not in st.session_state:
+        st.session_state.upload_link_ref_str = ""
+    if "df_import" not in st.session_state:
+        st.session_state.df_import = pd.DataFrame()
+
+
+session_state_init()
+
 
 st.warning(
     "Scraping is time intensive! To check out an analysis without waiting, try a provided dataset.",
